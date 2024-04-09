24eba87de80a4c73a9b51097a088fe08e4581645
brixxt27 <brixxt27@gmail.com>
2024-04-09 22:10:38 +0900

    chore: init mongodb

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const MONGODB_URI = 'mongodb+srv://jayoon:jayoon1234@boiler-plate.7nn0qqd.mongodb.net/?retryWrites=true&w=majority'
+
+const mongoose = require('mongoose')
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
